a19722c072f35d4d78c95a29a78c78a1e8e6070a
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-12-03 10:58:03 +0100

    Implement layout for header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '../styles/globals.css';
+import { css, Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            text-decoration: none;
+          }
+          html,
+          body {
+            font-family: 'Arial', sans-serif;
+            font-size: 16px;
+            line-height: 1.4;
+            display: flex;
+            flex-direction: column;
+            @media screen and (max-width: 600px) {
+              position: absolute;
+              min-height: 100vh;
+            }
+          }
+        `}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
